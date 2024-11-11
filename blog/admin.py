9bfcb927e7f20f1b79bf2b258a019ad1b6326355
Thiago <tgkshima@usp.br>
2024-11-11 15:34:06 -0300

    Added Class Post; Admin User and .gitgnore

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from blog.models import  Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
